83a7614c4f65585291e65c6975af18589dd99973
DJH ("illudium") <david@coresolutiongroup.com>
2021-11-23 17:08:53 -0500

    Update chrome_version.ps1

--- chrome_version.ps1
+++ chrome_version.ps1
@@ -1,5 +1,6 @@
 $exe = "C:\Program Files*\Google\Chrome\Application\chrome.exe"
-if (Test-Path $exe) {
+$path = (Resolve-Path $exe).ProviderPath
+if (Test-Path $path) {
             $path = (Resolve-Path $exe).ProviderPath
             $ver = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($path).FileVersion
         }       
