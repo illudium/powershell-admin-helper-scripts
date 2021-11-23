eba5b669c5219d671fd0ff30bbf2f8ecef090b0a
DJH ("illudium") <david@coresolutiongroup.com>
2021-11-23 16:37:46 -0500

    Update acrobatReaderVersion.ps1

--- acrobatReaderVersion.ps1
+++ acrobatReaderVersion.ps1
@@ -1,5 +1,6 @@
 $exe = "C:\Program Files*\Adobe\Acrobat *\Acrobat\Acrobat.exe"
-if (Test-Path $exe) {
+$path = (Resolve-Path $exe).ProviderPath
+if (Test-Path $path) {
   $AdobeInfo = Get-ItemProperty "HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | where {$_.DisplayName -like 'Adobe Acrobat*' -and $_.DisplayName -NotLike '*Reader*'}
   $acro_prod = (Get-ItemProperty "HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | where {$_.DisplayName -like 'Adobe Acrobat*' -and $_.DisplayName -NotLike '*Reader*'} | select-object -ExpandProperty DisplayName | %{$_.Split() | Select -Last 2}) -join ' '
   $acro_ver =  (Get-ItemProperty "HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | where {$_.DisplayName -like 'Adobe Acrobat*' -and $_.DisplayName -NotLike '*Reader*'} | select-object -ExpandProperty DisplayVersion | %{$_.Split('.') | Select -First 3}) -join '.'
