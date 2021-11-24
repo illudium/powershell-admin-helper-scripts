ca5bb3cf110c1409008ee52fff074c10a1ec7c73
DJH ("illudium") <david@coresolutiongroup.com>
2021-11-24 10:32:00 -0500

    Update acrobatReaderVersion.ps1
    Clean up and followup leveraging intentional improvement(s) by properly using the first defined $variable.

--- acrobatReaderVersion.ps1
+++ acrobatReaderVersion.ps1
@@ -4,7 +4,7 @@ if (Test-Path $path) {
   $AdobeInfo = Get-ItemProperty "HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | where {$_.DisplayName -like 'Adobe Acrobat*' -and $_.DisplayName -NotLike '*Reader*'}
   $acro_prod = ($AdobeInfo | select-object -ExpandProperty DisplayName | %{$_.Split() | Select -Last 2}) -join ' '
   $acro_ver =  ($AdobeInfo | select-object -ExpandProperty DisplayVersion | %{$_.Split('.') | Select -First 3}) -join '.'
-  $installDate = ($AdobeInfo | where {$_.DisplayName -like 'Adobe Acrobat*' -and $_.DisplayName -NotLike '*Reader*'} | select-object -ExpandProperty InstallDate).Insert(4,'-').Insert(7,'-')       
+  $installDate = ($AdobeInfo | select-object -ExpandProperty InstallDate).Insert(4,'-').Insert(7,'-')       
 } 
 
 else { $acro_prod = "n/a" }
