3680759f40d83ff59363967450dda7f921cd0f04
DJH ("illudium") <david@coresolutiongroup.com>
2022-02-19 10:36:13 -0500

    Update AltaroBackup_result.ps1

--- AltaroBackup_result.ps1
+++ AltaroBackup_result.ps1
@@ -1,10 +1,10 @@
 # ADJUST LINE BELOW to account for the name of the VM for which you want backup result status
-# Script must run the same day as the backup(s) occur, or adjust $tStr according to your needA
+# Script must run the same day as the backup(s) occur, or adjust $tStr according to your needs
 
 $serverName = "*File*Server*"
 $tStr = (get-date -Format o).Split("T")[0]
 $path = "C:\ProgramData\Altaro\AltaroBackupProfile\Logs\OpControllers"
-$srvrBk = Get-ChildItem -Path $path\* -Include $tStr*$serverName*_Success.log | sls "ControllerRct.DoWork\(\)\: CONTROLLER RESULT"
+$srvrBk = Get-ChildItem -Path $path\* -Include $tStr*$serverName* | Where-Object {($_.Name -match "_Failed" -or "Success") } | sls "CONTROLLER RESULT"
 $srvBkResult = $srvrBk.Line.split('=')[1]
 
 ## Additional code to log result or include in a generated email
